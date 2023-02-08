30af670ad070affe485b12156bfa1189875e6d31
CassiniSea <CassiniSeaandroid@gmail.com>
2023-02-08 19:26:58 +0200

    UART_RECEIVE_STRINGS_ENABLE -> UART_RECEIVE_STRING_ENABLE

--- interrupts.c
+++ interrupts.c
@@ -1,7 +1,7 @@
 #include "stm8s.h"
 #include "uart.h"
 
-#if defined(UART_RECEIVE_STRINGS_ENABLE) || defined(UART_RECEIVE_BYTE_ENABLE)
+#if defined(UART_RECEIVE_STRING_ENABLE) || defined(UART_RECEIVE_BYTE_ENABLE)
 	INTERRUPT void UART_RECEIVE8_INTERRUPT_VECTOR(void)	{
 		UART1_ClearITPendingBit(UART1_IT_RXNE);
 		uartReceive8(UART1_ReceiveData8());
--- uart.h
+++ uart.h
@@ -1,21 +1,21 @@
 /*
-If you want to receive STRINGS by uart
-define a UART_RECEIVE_STRINGS_ENABLE and
+If you want to receive STRING by uart
+define a UART_RECEIVE_STRING_ENABLE and
 define a void uartStringReceived(char* str) function
 
 else if you want receive a BYTE by uart
 define a UART_RECEIVE_BYTE_ENABLE and
 define a void uartReceive8(uint8_t c) function
 */
-#define UART_RECEIVE_STRINGS_ENABLE
+#define UART_RECEIVE_STRING_ENABLE
 #define UART_MAX_STRING_LENGTH 32
 //#define UART_RECEIVE_BYTE_ENABLE
 
 #ifdef UART_RECEIVE_BYTE_ENABLE
-	#undef UART_RECEIVE_STRINGS_ENABLE
+	#undef UART_RECEIVE_STRING_ENABLE
 #endif 
 
-#if defined(UART_RECEIVE_STRINGS_ENABLE) || defined(UART_RECEIVE_BYTE_ENABLE)
+#if defined(UART_RECEIVE_STRING_ENABLE) || defined(UART_RECEIVE_BYTE_ENABLE)
 	#define UART_RECEIVE8_INTERRUPT_VECTOR uartReceive8Interrupt
 #else
 	#define UART_RECEIVE8_INTERRUPT_VECTOR NonHandledInterrupt
